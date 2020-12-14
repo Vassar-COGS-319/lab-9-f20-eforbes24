a821437be8a4300f8711883cd875168b9b6415a5
eforbes24 <44680407+eforbes24@users.noreply.github.com>
2020-12-14 10:47:03 -0500

    Update 2-feature-embeddings.R

--- 2-feature-embeddings.R
+++ 2-feature-embeddings.R
@@ -1,4 +1,4 @@
-Sys.setenv('CUDA_VISIBLE_DEVICES' = "0")
+Sys.setenv('CUDA_VISIBLE_DEVICES' = "6")
 
 # STAGE 2: Feature Embeddings
 
@@ -18,17 +18,21 @@ images.preprocessed <- imagenet_preprocess_input(data$images, mode="tf")
 # feature layers. include_top = T will include the two dense layers at
 # the top of the network, plus the categorization layer.
 
-# fill this in
+conv_base <- application_vgg16(input_shape=c(128,128,3), include_top=FALSE)
+summary(conv_base)
 
 # We can setup a model that incorporated the VGG16 model, but with a 
 # flatten layer at the top to turn the feature maps into a single
 # vector of features, which will be easier to use in our MINERVA model.
 
-# fill this in
+model <- keras_model_sequential()
+model %>% 
+  conv_base %>% 
+  layer_flatten()
 
 # Get the features by running predict()
 
-# fill this in
+features <- model %>% predict(images.preprocessed)
 
 # Save the resulting output
 saveRDS(features, file="scene-features-vgg16-128.rds")
